245e52cf522a3834344bfc11afa8b090aadcc45e
Marcus Iden <marcus.iden@chemringnobel.no>
2026-08-10 10:12:35 +0200

    enda en endring ei env

--- Notebook_Dummy_Silver.Notebook/notebook-content.py
+++ Notebook_Dummy_Silver.Notebook/notebook-content.py
@@ -40,6 +40,8 @@ from pyspark.sql.functions import col, when, upper, current_timestamp
 
 import notebookutils
 
+from pyspark.sql.functions import lit
+
 vl = notebookutils.variableLibrary.getLibrary("Variables_Test")
 env = vl.Environment_Name
 print(f"Kjører i miljø: {env}")
@@ -76,6 +78,15 @@ silver_df = (
     # Drop any rows with a null country (basic data-quality gate)
     .filter(col("country").isNotNull())
 )
+
+
+vl = notebookutils.variableLibrary.getLibrary("Variables_Test")
+env = vl.Environment_Name
+print(f"Kjører i miljø: {env}")
+
+# skriv den inn i dataene så du ser det i tabellen etterpå
+silver_df = silver_df.withColumn("env_stamp", lit(env))
+
 
 # 3. Write to the silver table (overwrite for idempotent re-runs)
 silver_df.write.mode("overwrite").format("delta").saveAsTable("silver_customers")
